b817965929b883525755a68d87a6ebf7088ec1dc
Meng Hak <meng.hak@xcentium.com>
2023-02-04 17:32:00 -0800

    prereq

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Sitecore Next.js Guide</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/mhak/sitecore-nextjs-guide',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/mhak/sitecore-nextjs-guide',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Sitecore Next.js Guide',
   },
 }
 
