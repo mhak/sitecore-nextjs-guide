1f8f7b4791da8491288dc64041cab24f719293d8
Meng Hak <meng.hak@xcentium.com>
2023-02-05 17:15:38 -0800

    add page suffix; clean up

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,10 +3,15 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Sitecore Next.js Guide</span>,
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Sitecore Next.js Guide'
+    }
+  },
   project: {
     link: 'https://github.com/mhak/sitecore-nextjs-guide',
   },
-  docsRepositoryBase: 'https://github.com/mhak/sitecore-nextjs-guide',
+  docsRepositoryBase: 'https://github.com/mhak/sitecore-nextjs-guide/tree/main',
   footer: {
     text: 'Sitecore Next.js Guide',
   },
