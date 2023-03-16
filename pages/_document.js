df5022b370b8f9b8eaf2ea5865fcfa3589502a6d
Meng Hak <meng.hak@xcentium.com>
2023-03-15 21:20:02 -0700

    GA

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=G-MG47WVEFHL`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-MG47WVEFHL');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
