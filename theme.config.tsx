f16692bd5d6957291ec418cebdb15ae413eaea4c
Meng Hak <meng.hak@xcentium.com>
2023-05-06 13:30:47 -0700

    scs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import { useConfig } from 'nextra-theme-docs'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
@@ -10,13 +9,10 @@ const config: DocsThemeConfig = {
     let path = asPath === '/'
       ? ''
       : asPath;
-    const { frontMatter } = useConfig();
     const canonicalUrl = `https://sitecore-nextjs-guide.hakmeng.com${path}`;
     return <>
       <link rel="canonical" href={canonicalUrl} />
       <meta property="og:url" content={canonicalUrl} />
-      <meta property="og:title" content={frontMatter.title} />
-      <meta property="og:description" content={frontMatter.description} />
     </>
   },
   useNextSeoProps() {
