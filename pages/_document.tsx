08f04bd96d0df0515588dab96eae78df9c63416b
Meng Hak <meng.hak@xcentium.com>
2023-05-11 17:06:42 -0700

    reusable data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang='en-US'>
         <Head>
+          <link rel="shortcut icon" href="/favicon.ico" type='image/x-icon'/>
           <script async src={`https://www.googletagmanager.com/gtag/js?id=G-MG47WVEFHL`} />
           <script
             dangerouslySetInnerHTML={{
