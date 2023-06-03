775d6e6ba36d64654bac179b55231b6b0bff1660
Meng Hak <meng.hak@xcentium.com>
2023-06-03 16:02:10 -0700

    sitemap changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://sitecore-nextjs-guide.hakmeng.com',
     generateRobotsTxt: true, // (optional)
-    // ...other options
+    generateIndexSitemap: false,
   }
