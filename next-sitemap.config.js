c189737c8cab234b9ab62c1e6b37c2e8a1992c88
Meng Hak <meng.hak@xcentium.com>
2023-06-03 17:00:18 -0700

    revert lastmod

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,24 +1,9 @@
-const { promises: fs } = require('fs');
-const { format } = require('date-fns');
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://sitecore-nextjs-guide.hakmeng.com',
-    generateRobotsTxt: true, // (optional)
-    generateIndexSitemap: false,
-    autoLastmod: false,
-    priority: false,
-    changefreq: false,
-    transform: async (config, path) => {
-      let file = path == "/"
-        ? "/index"
-        : path;
-      let computedPath = `./pages${file}.mdx`;
-      let lastmod = format((await fs.stat(computedPath)).mtime, "yyyy-MM-dd");
-      console.log(`${computedPath} ${lastmod}`);
-      return {
-        loc: path,
-        lastmod,
-      }
-    }
-  }
+  siteUrl: process.env.SITE_URL || 'https://sitecore-nextjs-guide.hakmeng.com',
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  autoLastmod: false,
+  priority: false,
+  changefreq: false
+}
